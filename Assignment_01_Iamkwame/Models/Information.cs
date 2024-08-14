83753b31d15399d3e92175fc5f680e088bafd833
Sey George Bernasko Osei <102140991+seygeorge@users.noreply.github.com>
2024-08-14 22:43:26 +0000

    blazor_forms

---
+++ Assignment_01_Iamkwame/Models/Information.cs
@@ -0,0 +1,17 @@
+﻿using Assignment_01_Iamkwame.Components;
+using Microsoft.AspNetCore.Components;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_01_Iamkwame.Models
+{
+    public class Information
+    {
+
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Password { get; set; } 
+
+       
+    }
+}
